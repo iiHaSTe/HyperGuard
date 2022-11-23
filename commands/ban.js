0adf3c4f235fbc4026f10ea48b8e2600a9b34aa7
iiHaSTe <www.hicham2005@gmail.com>
2022-11-23 21:38:02 +0100

    add ban command

---
+++ commands/ban.js
@@ -0,0 +1,40 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  Interaction,
+  PermissionFlagsBits: Permission,
+  User
+} = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("ban")
+    .setDescription("ban a bad guy")
+    .addUserOption(opt => opt
+      .setName("user")
+      .setDescription("member you want to ban")
+      .setRequired(true))
+    .setDefaultMemberPermissions(Permission.BanMembers)
+    .setDMPermission(false),
+
+  /** @param {Interaction} inter */
+  async execute(inter){
+    const {guild, options, client} = inter;
+    /** @param {User} */
+    const user = options.getUser("user");
+
+    let embed = new EmbedBuilder()
+      .setTitle(guild.name)
+      .setThumbnail(guild.iconURL() ?? client.user.avatarURL())
+      .setDescription(`this guy <@${user.id}> are a bad guy ok.. lets ban him >:) :hammer:`)
+      .setColor([255, 0, 0])
+      .setFooter({
+        text: "Powred by "+client.user.username,
+        iconURL: client.user.avatarURL()
+      });
+    await inter.reply({embeds: [embed]});
+    await guild.members.ban(user);
+    await inter.channel.send(`>>> **${user.username}** are \`banned\` :hammer:\nid: || ${user.id} ||`);
+  }
+}
+
